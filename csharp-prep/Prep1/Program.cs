96d96654e8c33e8b10a3421571e62da822e0ee01
HenryBondah <bondahdean@gmail.com>
2023-04-19 10:51:40 -0600

    Prep 1 and 2 with some examples in sandbox

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("Welcome to CSE 210");
+        Console.WriteLine("Are you ready to answer some few Questions? Then Lets Go!!");
+        Console.Write("What is your Firstname : ");
+        String Firstname = Console.ReadLine();
+
+        Console.Write("what is your Lastname : ");
+        string Lastname = Console.ReadLine();
+        Console.WriteLine($"You provided '{Firstname}' as your first name and '{Lastname}' as your last name,");
+        Console.WriteLine($"So you are : {Firstname} {Lastname}.");
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+
+        Console.Write("enter a number: ");
+        string number = Console.ReadLine();
+
+        int x = int.Parse(number);
+        int y = 6;
+
+        if (x > y)
+        {
+            Console.WriteLine("Greater");
+        }
+        else if (x < y)
+        {
+            Console.WriteLine("Less");
+        }
+        else
+        {
+            Console.WriteLine("Equal");
+        }
+        
+
+        Console.WriteLine("Tell me Name?");
+        string name = Console.ReadLine();
+        Console.WriteLine($"Your name is {name}");
+
+        int Number =10;
+        Number = Number + 4;
+        Console.WriteLine($" Alright {name}, I guess your magic number is ");
+        Console.WriteLine(Number);
+        
+
+        Console.WriteLine("write a number");
+        string word = Console.ReadLine();
+        int NumberEntered = int.Parse(number);
+        Console.WriteLine($"You typed {number}");
+        Console.WriteLine($"This and your magic number will Magically.,m/m.,/,/.,/m be {Number + number}");
+
     }
 }
